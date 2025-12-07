8e75d73db349155543efd512aa97af852c5f8708
FFengIll <fengyouzheng@gmail.com>
2025-12-07 15:13:58 +0800

    feat: complete basic GUI version with wails3.

---
+++ wails3/app.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+	services2 "tingly-box/wails3/services"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+)
+
+const (
+	AppName        = "Tingly Box"
+	AppDescription = "A proxy server for AI model APIs with web UI"
+)
+
+var App *application.App
+
+func newApp() *application.App {
+	// Create UI service
+	uiService, err := services2.NewUIService()
+	if err != nil {
+		log.Fatalf("Failed to create UI service: %v", err)
+	}
+
+	// Create a new Wails application by providing the necessary options.
+	// Variables 'Name' and 'Description' are for application metadata.
+	// 'Assets' configures the asset server with the 'FS' variable pointing to the frontend files.
+	// 'Services' is a list of Go struct instances. The frontend has access to the methods of these instances.
+	// 'Mac' options tailor the application when running an macOS.
+	ginEngine := uiService.GetGin()
+	embdHandler := application.AssetFileServerFS(assets)
+	app := application.New(application.Options{
+		Name:        AppName,
+		Description: AppDescription,
+		Services: []application.Service{
+			application.NewService(&services2.GreetService{}),
+			application.NewService(uiService),
+		},
+		Assets: application.AssetOptions{
+			Middleware: func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					if strings.HasPrefix(r.URL.Path, "/api") {
+						// Let API handle everything else
+						ginEngine.ServeHTTP(w, r)
+						return
+					}
+
+					// Let Wails handle the `/wails/` route
+					if strings.HasPrefix(r.URL.Path, "/dashboard") {
+						next.ServeHTTP(w, r)
+						return
+					}
+
+					embdHandler.ServeHTTP(w, r)
+					return
+				})
+			},
+		},
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+		Windows: application.WindowsOptions{},
+		SingleInstance: &application.SingleInstanceOptions{
+			UniqueID: "tingly-box.single-instance",
+			OnSecondInstanceLaunch: func(data application.SecondInstanceData) {
+				if WindowMain != nil {
+					WindowMain.EmitEvent("secondInstanceLaunched", data)
+					WindowMain.Restore()
+					WindowMain.Focus()
+				}
+			},
+			AdditionalData: map[string]string{
+				"launchtime": time.Now().Local().String(),
+			},
+			ExitCode:      0,
+			EncryptionKey: [32]byte([]byte("Ml!Zjj@Lfw#Wqq$Wxb%Mjy^&*()_+1234567890-=")[:32]),
+		},
+	})
+
+	return app
+}
