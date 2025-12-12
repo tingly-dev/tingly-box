bcf3fc28a550cbe868363abb6223e9292e201a12
FFengIll <fengyouzheng@gmail.com>
2025-12-12 15:04:54 +0800

    codeclean: for wails.

--- wails3/main.go
+++ wails3/main.go
@@ -4,12 +4,6 @@ import (
 	"embed"
 	_ "embed"
 	"log"
-	"net/http"
-	"strings"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"github.com/wailsapp/wails/v3/pkg/application"
 )
 
 // Wails uses Go's `embed` package to embed the frontend files into the binary.
@@ -24,22 +18,7 @@ func init() {
 	// Register a custom event whose associated data type is string.
 	// This is not required, but the binding generator will pick up registered events
 	// and provide a strongly typed JS/TS API for them.
-	application.RegisterEvent[string]("time")
-}
-
-// GinMiddleware creates a middleware that passes requests to Gin if they're not handled by Wails
-func GinMiddleware(ginEngine *gin.Engine) application.Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Let Wails handle the `/wails` route
-			if strings.HasPrefix(r.URL.Path, "/") {
-				next.ServeHTTP(w, r)
-				return
-			}
-			// Let Gin handle everything else
-			ginEngine.ServeHTTP(w, r)
-		})
-	}
+	// application.RegisterEvent[string]("time")
 }
 
 // main function serves as the application's entry point. It initializes the application, creates a window,
@@ -52,13 +31,13 @@ func main() {
 
 	// Create a goroutine that emits an event containing the current time every second.
 	// The frontend can listen to this event and update the UI accordingly.
-	go func() {
-		for {
-			now := time.Now().Format(time.RFC1123)
-			app.Event.Emit("time", now)
-			time.Sleep(time.Second)
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		now := time.Now().Format(time.RFC1123)
+	// 		app.Event.Emit("time", now)
+	// 		time.Sleep(time.Second)
+	// 	}
+	// }()
 
 	// Run the application. This blocks until the application has been exited.
 	err := app.Run()
