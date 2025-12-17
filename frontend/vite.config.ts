f8a65ea231505385e283c45d6d361709638ac572
FFengIll <fengyouzheng@gmail.com>
2025-12-17 17:20:07 +0800

    build: update frontend build.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,27 @@
 import react from '@vitejs/plugin-react-swc';
 import wails from "@wailsio/runtime/plugins/vite";
-import { defineConfig } from 'vite';
-import { viteMockServe } from 'vite-plugin-mock';
+import {defineConfig} from 'vite';
+import {viteMockServe} from 'vite-plugin-mock';
 
 // Check if we should use mock data
 const useMock = process.env.USE_MOCK === 'true'
 console.log("use mock", useMock)
 
 // Check if we're building for GUI mode
-const isGuiMode = (process.env.VITE_PKG_MODE === 'gui')
+const useGUI = process.env.USE_GUI === 'true'
+console.log("use gui", useGUI)
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        // Only include wails plugin when building for GUI mode
-        ...(isGuiMode ? [wails("./src/bindings")] : []),
         react(),
-        viteMockServe({
+        // Only include wails plugin when building for GUI mode
+        ...(useGUI ? [wails("./src/bindings")] : []),
+        ...(useMock ? [viteMockServe({
             mockPath: 'src/mock',
             enable: useMock,
             logger: true,
-        })
+        })] : []),
     ],
     server: {
         proxy: useMock ? {} : {
