0fa96d7e5772e5c4e55883e1610be2b50eba5212
FFengIll <fengyouzheng@gmail.com>
2026-01-17 09:18:22 +0800

    refactor: mv.

--- internal/smart_routing/type.go
+++ internal/smart_routing/type.go
@@ -7,48 +7,8 @@ import (
 // SmartOpPosition represents the field to check in a request for smart routing
 type SmartOpPosition string
 
-const (
-	PositionModel    SmartOpPosition = "model"    // Request model name
-	PositionThinking SmartOpPosition = "thinking" // Thinking mode enabled
-	PositionSystem   SmartOpPosition = "system"   // System message content
-	PositionUser     SmartOpPosition = "user"     // User message content
-	PositionToolUse  SmartOpPosition = "tool_use" // Tool use/name
-	PositionToken    SmartOpPosition = "token"    // Token count
-)
-
 // SmartOpOperation represents the operation to perform on a position
 type SmartOpOperation string
-
-const (
-	// Model operations
-	OpModelContains SmartOpOperation = "contains" // Model name contains the value
-	OpModelGlob     SmartOpOperation = "glob"     // Model name matches glob pattern
-	OpModelEquals   SmartOpOperation = "equals"   // Model name equals the value
-
-	// Thinking operations
-	OpThinkingEnabled  SmartOpOperation = "enabled"  // Thinking mode is enabled
-	OpThinkingDisabled SmartOpOperation = "disabled" // Thinking mode is disabled
-
-	// System message operations
-	OpSystemAnyContains SmartOpOperation = "any_contains" // Any system messages contain the value
-	OpSystemRegex       SmartOpOperation = "regex"        // Any system messages match regex pattern
-
-	// User message operations
-	OpUserAnyContains SmartOpOperation = "any_contains" // Any user messages contain the value
-	OpUserContains    SmartOpOperation = "contains"     // Latest message is `user` role and it contains the value
-	OpUserRegex       SmartOpOperation = "regex"        // Combined user messages match regex pattern
-	OpUserRequestType SmartOpOperation = "type"         // Latest message is `user` role and check its content type
-
-	// Tool use operations
-	OpToolUseIs       SmartOpOperation = "is"       // Latest message is `tool use` and its name is the value
-	OpToolUseContains SmartOpOperation = "contains" // Latest message is `tool use` and its name or arguments contains the value
-
-	// Token operations
-	OpTokenGe SmartOpOperation = "ge" // Token count greater than or equal to value
-	OpTokenGt SmartOpOperation = "gt" // Token count greater than value
-	OpTokenLe SmartOpOperation = "le" // Token count less than or equal to value
-	OpTokenLt SmartOpOperation = "lt" // Token count less than value
-)
 
 // SmartOpValueType represents the type of value in a smart routing operation
 type SmartOpValueType string
